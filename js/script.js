c4cc67b60b6e6a5be5bec40101f522e5c0114b3e
Davide <davide.vignozzi@gmail.com>
2021-01-21 17:01:01 +0100

    Perchè due icone?

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@
 // - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
 
 const icons = [
-  { name: 'comment', prefix: 'fa fa-comment', type: '', family: 'reaction', },
-  { name: 'thumbs', prefix: 'fa fa-thumbs-up ', type: '', family: 'reaction', },
-  { name: 'heart', prefix: 'fa fa-heart', type: '', family: 'reaction', },
+  { name: 'comment', prefix: 'fa fa-comment', type: 'reaction', family: '', },
+  { name: 'thumbs', prefix: 'fa fa-thumbs-up ', type: 'reaction', family: '', },
+  { name: 'heart', prefix: 'fa fa-heart', type: 'reaction', family: '', },
 
-  { name: 'car', prefix: 'fa fa-car', type: '', family: 'transport', },
-  { name: 'plane', prefix: 'fa fa-plane', type: '', family: 'transport', },
-  { name: 'train', prefix: 'fa fa-train', type: '', family: 'transport', },
-  { name: 'subway', prefix: 'fa fa-subway', type: '', family: 'transport', },
+  { name: 'car', prefix: 'fa fa-car', type: 'transport', family: '', },
+  { name: 'plane', prefix: 'fa fa-plane', type: 'transport', family: '', },
+  { name: 'train', prefix: 'fa fa-train', type: 'transport', family: '', },
+  { name: 'subway', prefix: 'fa fa-subway', type: 'transport', family: '', },
 
-  { name: 'ambulance', prefix: 'fa fa-ambulance', type: '', family: 'healty', },
-  { name: 'stethoscope', prefix: 'fa fa-stethoscope', type: '', family: 'transport', },
-  { name: 'medkit', prefix: 'fa fa-medkit', type: '', family: 'transport', },
+  { name: 'ambulance', prefix: 'fa fa-ambulance', type: 'healty', family: '', },
+  { name: 'stethoscope', prefix: 'fa fa-stethoscope', type: 'healty', family: '', },
+  { name: 'medkit', prefix: 'fa fa-medkit', type: 'healty', family: '', },
 ]
 
-const appContainer = document.getElementById('container');
+const appContainer = document.getElementById('container'); // prendo il container
 
 icons.forEach((element) => {
 
@@ -33,6 +33,46 @@ icons.forEach((element) => {
 // Milestone 2:
 // - Definire un array di colori e associare ad ogni tipo di icona un colore.
 // - Visualizzare le icone di colore diverso in base al tipo.
+
+const colors = [
+  {color: 'blue', type: 'reaction'},
+  {color: 'green', type: 'transport'},
+  {color: 'red', type: 'healty'},
+]
+
+const newIcons = icons.map((element) => {
+
+  // reaction === blue
+  if (element.type === 'reaction') {
+    return {...element, 'color': 'blue'}
+  }
+
+  // transport === green
+  if (element.type === 'transport') {
+    return {...element, 'color': 'green'}
+  }
+
+  // healty === red
+  if (element.type === 'healty') {
+    return {...element, 'color': 'red'}
+  }
+
+})
+
+const appContainerSecond = document.getElementById('container-second'); //prendo il secondo container
+
+newIcons.forEach((element) => {
+
+  appContainerSecond.innerHTML +=
+  `
+  <div>
+    <i class="${element.prefix}" style="color:${element.color}"</i>
+    ${element.name}
+  </div>
+  `
+
+})
+
 
 
 // Milestone 3:
